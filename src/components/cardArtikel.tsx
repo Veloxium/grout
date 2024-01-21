77eba59cd0bb5b0969dc9dad9708d051f9c1fb56
Veloxium <veloxium.09@gmail.com>
2024-01-21 23:59:38 +0800

    fix image issue in artikel

--- src/components/cardArtikel.tsx
+++ src/components/cardArtikel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import Jo from "@/assets/images/jo.webp";
+import CardImg from "@/assets/images/artikel/3.png";
 import Link from "next/link";
 
 function CardArtikel() {
@@ -24,11 +24,9 @@ function CardArtikel() {
       </div>
       <div className="w-1/2 py-4 flex items-center">
         <Image
-          src={
-            "https://s3-alpha-sig.figma.com/img/649a/7c31/57e387d433221829a98d46adbd4bafd0?Expires=1704672000&Signature=nWPJ-FGQhaG662zdIIlgR5cayfk~ku1cz6amNIiNgTZ18sUvI-CErOh5Kry9yUUxD25TR1qRqk7Le3WuyovmjgM0xVN3rOL6PiMtGipCbooF3p6Z~4rZU5KE6HMX5O6Vp8zCgvZ6lyZsaSAiVjUdiydxX9FF4fVQys8Z~zbUAsnedYFrsBfUDcKOoZ9lSCUC5GuOXJL4uP4LmjrzLXNUQQF-Yvj9jAGnMQdBMKX6zaVg7hy3Dr-egNkKSxfxRrkYFgoZ0Q~eUaBvJ3lVt2IPB46ayveFpZm6dKCa~HpE4IZuR2fxEZX~tmGtkjZRiCxzSeuN9YTzKJkpXr-oOPOd~w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-          }
-          width={900}
-          height={900}
+          src={CardImg}
+          width={1000}
+          height={1000}
           alt="jo"
           className="w-full object-cover h-[250px]"
         />
